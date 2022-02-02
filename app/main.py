6be2c7e1803435675c8a9e0cb3d101e4cc5385bf
RodrigoCursino <rodrigoapmc@gmail.com>
2022-02-02 19:05:41 -0300

    index.html

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi                 import FastAPI, Request
+from fastapi.templating      import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# uvicorn src.app:api --reload --reload-dir=src
 
+# CORS
+origins = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8080'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# index build files
+templates = Jinja2Templates(directory="app/templates") 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World Heroku"}
+async def serve_home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
